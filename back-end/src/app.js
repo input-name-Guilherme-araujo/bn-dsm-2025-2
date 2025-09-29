3bf24b36e5bd5b552bcce2b3825ecc577b31682b
Guilherme de Araujo Silva <guilhermedeearaujo@gmail.com>
2025-09-29 03:43:38 -0300

    (22/09) Finalizados CRUDs de categorias e clientes

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -18,6 +18,9 @@ app.use('/users', usersRouter)
 /******************** ROTAS ********************/
 import categoriasRouter from './routes/categorias.js'
 app.use('/categorias', categoriasRouter)
+
+import clientesRouter from './routes/clientes.js'
+app.use('/clientes', clientesRouter)
 /***********************************************/
 
 export default app
